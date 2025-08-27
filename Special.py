6f1f149400e9dd94fe398c032ce6aaadf3509aed
hlkay <heinlinkyaw92@gmail.com>
2025-08-27 17:43:02 +0630

    Update Special.py

--- Special.py
+++ Special.py
@@ -17,7 +17,7 @@ messages = [
     "You’re the best thing that ever happened to me ❤️",
 ]
 
-# Keep track of how many times she clicked
+# Keep track of clicks
 if "clicks" not in st.session_state:
     st.session_state.clicks = 0
 
@@ -26,8 +26,7 @@ if st.button("💌 Click me for a surprise"):
     if st.session_state.clicks < 5:
         st.success(random.choice(messages))
     else:
-        st.balloons()
         st.markdown(
-            "<h2 style='text-align:center; color:#ff1493;'>Final Message: I Love You Forever ❤️</h2>",
+            "<h2 style='text-align:center; color:#ff1493;'>Final Message: I Worked my ass off for this just for you. ❤️</h2>",
             unsafe_allow_html=True,
         )
